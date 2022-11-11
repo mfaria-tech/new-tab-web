d7abca2c5084ca7cc0789f3405f9fe2da1cc4985
Marcus Faria <mfaria150.github@gmail.com>
2022-11-11 11:33:34 -0300

    Moved shortcuts tags to index document. Inited scripts to shortcut buttons. Added Dialog tag and style.

---
+++ scripts/shortcuts.js
@@ -0,0 +1,18 @@
+/*
+ * TITLE:   shortcuts.js
+ * AUTOR:   mfaria-tech
+ * DATE:    11.11.2022
+ * 
+ * DESCRIPTION:
+ *      roles and actions to shortcut buttons
+ */
+
+
+// Sintaxe strict mode para todo o script
+"use strict";
+
+// Added event on click to button plus
+const BTN_PLUS = document.getElementById("link__shortcut-plus");
+const POPUP = document.getElementById("dialog__form");
+
+alert(POPUP.textContent);
